c6abc15d5dc81eecacf6a5d71f861caab698eb1a
Tom Wyant <harryfmudd@comcast.net>
2026-06-08 10:52:15 -0400

    Replace wyant@cpan.org with harryfmudd@comcast.net.

--- inc/My/Module/Meta.pm
+++ inc/My/Module/Meta.pm
@@ -26,7 +26,7 @@ sub add_to_cleanup {
 }
 
 sub author {
-    return 'Thomas R. Wyant, III F<wyant at cpan dot org>';
+    return 'Thomas R. Wyant, III F<harryfmudd at comcast dot net>';
 }
 
 sub build_requires {
@@ -70,7 +70,7 @@ sub meta_merge {
 	    bugtracker	=> {
 		web	=> 'https://rt.cpan.org/Public/Dist/Display.html?Name=File-Symlink-Relative',
 		# web	=> 'https://github.com/trwyant/perl-File-Symlink-Relative/issues',
-		mailto  => 'wyant@cpan.org',
+		mailto  => 'harryfmudd@comcast.net',
 	    },
 	    license	=> 'http://dev.perl.org/licenses/',
 	    repository	=> {
@@ -338,7 +338,7 @@ electronic mail to the author.
 
 =head1 AUTHOR
 
-Thomas R. Wyant, III F<wyant at cpan dot org>
+Thomas R. Wyant, III F<harryfmudd at comcast dot net>
 
 =head1 COPYRIGHT AND LICENSE
 
